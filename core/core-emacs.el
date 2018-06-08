2c36e25af111ce24ab2b15dcd94f597304c6f2d0
renchunhui <346878847@qq.com>
2018-06-08 22:25:00 +0800

    first commit

---
+++ core/core-emacs.el
@@ -0,0 +1,28 @@
+;; core-emacs.el --- My personal Emacs configuration.
+;;
+;; Copyright (c) 2018
+;;
+;; Author  : Chunhui Ren <renchunhui2008@gmail.com>
+;; Repo    : https://github.com/RenChunhui/.emacs.d
+;;
+;; License: GPLv3
+;;
+
+(require 'core-packages)
+
+(defun emacs/init ()
+  "Initialization."
+  (emacs/remove-gui-elements))
+
+(defun emacs/remove-gui-elements ()
+  "Remove the tool bar and scroll bars."
+  (when (and (fboundp 'tool-bar-mode) (not (eq tool-bar-mode -1)))
+    (tool-bar-mode -1))
+  (when (and (fboundp 'scroll-bar-mode) (not (eq scroll-bar-mode -1)))
+    (scroll-bar-mode -1))
+  (when (and (fboundp 'tooltip-mode) (not (eq tooltip-mode -1)))
+    tooltip-mode -1))
+
+(emacs/init)
+
+(provide 'core-emacs)
---
+++ lisp/init-html.el
@@ -0,0 +1,29 @@
+(use-package web-mode
+  :ensure t
+  :mode ("\\.html$" . web-mode)
+  :init
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-auto-expanding t)
+  (setq web-mode-enable-css-colorization t))
+
+(use-package web-beautify
+  :ensure t
+  :commands (web-beautify-css
+             web-beautify-css-buffer
+             web-beautify-html
+             web-beautify-html-buffer
+             web-beautify-js
+             web-beautify-js-buffer))
+
+(use-package json-mode
+  :ensure t
+  :mode "\\.json\\'"
+  :config
+  (bind-key "{" #'paredit-open-curly json-mode-map)
+  (bind-key "}" #'paredit-close-curly json-mode-map))
+
+(provide 'init-html)
