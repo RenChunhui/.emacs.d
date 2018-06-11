e4bf885e98b7d08916d5ce90a14bedd0f2efb505
renchunhui <346878847@qq.com>
2018-06-11 15:36:55 +0800

    .emacs.d

--- core/core-emacs.el
+++ core/core-emacs.el
@@ -16,7 +16,8 @@
 (defun emacs/init ()
   "Initialization."
   (prefer-coding-system 'utf-8)
-  (emacs/remove-gui-elements))
+  (emacs/remove-gui-elements)
+  (emacs/display))
 
 (defun emacs/remove-gui-elements ()
   "Remove the tool bar and scroll bars."
@@ -26,6 +27,37 @@
     (scroll-bar-mode -1))
   (when (and (fboundp 'tooltip-mode) (not (eq tooltip-mode -1)))
     tooltip-mode -1))
+
+(defun emacs/display ()
+  ;; default font
+  (set-default-font "DroidSansMono Nerd Font-13")
+
+  ;; Warn when opening files bigger than 10MB
+  (setq large-file-warning-threshold 10000000)
+
+  ;; only type 'y' or 'n' instead of 'yes' or 'no'
+  (fset 'yes-or-no-p 'y-or-n-p)
+
+  ;; no splash screen
+  (setq inhibit-splash-screen t)
+
+  ;; no message on startup
+  (setq initial-scratch-message nil)
+  (setq fill-column 80)
+
+  ;; highlight current line
+  (global-hl-line-mode 1)
+  (global-linum-mode t)
+  
+  ;; PATH
+  (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+  (setq exec-path (append exec-path '("/usr/local/bin")))
+  
+  ;; nice fonts in macOS
+  (setq mac-allow-anti-aliasing t)
+  
+  ;; delete trailing whitespace in all modes
+  (add-hook 'before-save-hook #'delete-trailing-whitespace))
 
 (emacs/init)
 
