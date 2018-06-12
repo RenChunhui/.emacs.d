f1450b3cf79288506468efadd7b8d59ae715147f
renchunhui <346878847@qq.com>
2018-06-12 15:10:27 +0800

    .emacs.d

--- core/core-packages.el
+++ core/core-packages.el
@@ -10,4 +10,70 @@
 	(package-refresh-contents)
 	(package-install 'use-package))
 
+(use-package company
+    :ensure t
+    :config
+    (global-company-mode)
+    (setq company-idle-delay 0.1)
+    (setq company-tooltip-limit 10)
+    (setq company-minimum-prefix-length 2))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+;; ivy
+(use-package counsel
+  :ensure t
+  )
+
+(use-package swiper
+  :ensure t
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x C-f" . counsel-find-file))
+  :init
+  (setq ivy-use-virtual-buffers t)
+  :config
+  (ivy-mode 1))
+
+(use-package yasnippet
+  :ensure t
+  :init
+  (yas-global-mode 1))
+
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode t))
+
+;; projectile
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode t)
+  ;; To enable caching
+  (setq projectile-enable-caching t)
+  ;; Using projectile everywhere
+  (setq projectile-require-project-root nil)
+  (setq projectile-completion-system 'ivy))
+
+(use-package counsel-projectile
+  :ensure t)
+
+;; spaceline - A mode line
+(use-package spaceline
+  :ensure t
+  :init
+  (setq ns-use-srgb-colorspace nil)
+  (require 'spaceline-config)
+  (spaceline-spacemacs-theme)
+  :config
+  (setq powerline-default-separator 'slant))
+
+;; 括号自动补全
+(electric-pair-mode t)
+
 (provide 'core-packages)
+
+;;; core-packages ends here
--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; init.el --- My personal Emacs configuration.
+;;; init.el --- My personal Emacs configuration.
 ;;
 ;; Copyright (c) 2018
 ;;
@@ -26,3 +26,17 @@
 (require 'init-pdf)
 
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (spaceline org-bullets json-mode company-tern tern js2-mode scss-mode web-mode atom-one-dark-theme treemacs-projectile treemacs counsel-projectile projectile flycheck yasnippet counsel which-key company use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
