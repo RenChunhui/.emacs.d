721ecfe10fbc7a479a8a382bfc26a2b78757a60f
renchunhui <346878847@qq.com>
2018-06-13 18:05:53 +0800

    .emacs.d

--- core/core-emacs.el
+++ core/core-emacs.el
@@ -9,8 +9,30 @@
 ;; remove tooltip
 (tooltip-mode -1)
 
+;; 启用时间显示
+(display-time-mode t)
+
+;; 24 小时制
+(setq-default display-time-24hr-format t)
+
+;; 启用邮件设置
+(setq-default display-time-use-mail-icon t)
+
+;; 时间变化频率
+(setq-default display-time-interval 1000)
+
+(setq-default display-time-string-forms
+      '((propertize (format-time-string "%H:%M" now)
+		    'face 'bold)))
+
+;; 显示电池
+(display-battery-mode t)
+
+;; 不生成临时文件
+(setq-default make-backup-files nil)
+
 ;; default font
-(set-default-font "DroidSansMono Nerd Font-13")
+(set-frame-font "Droidsansmono Nerd Font-13")
 
 ;; Warn when opening files bigger than 10MB
 (setq large-file-warning-threshold 10000000)
@@ -27,6 +49,8 @@
 
 ;; highlight current line
 (global-hl-line-mode 1)
+
+;; 显示列号
 (global-linum-mode t)
 
 ;; PATH
@@ -36,9 +60,12 @@
 ;; nice fonts in macOS
 (setq mac-allow-anti-aliasing t)
 
+(setq ns-use-srgb-colorspace nil)
+
 ;; delete trailing whitespace in all modes
 (add-hook 'before-save-hook #'delete-trailing-whitespace)
 
+;; 显示括号匹配
 (electric-pair-mode t)
 
 (provide 'core-emacs)
--- init.el
+++ init.el
@@ -8,14 +8,18 @@
 (require 'core-packages)
 (require 'core-themes)
 (require 'core-dev)
+(require 'core-mode-line)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("cea3ec09c821b7eaf235882e6555c3ffa2fd23de92459751e18f26ad035d2142" "c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" "ef403aa0588ca64e05269a7a5df03a5259a00303ef6dfbd2519a9b81e4bce95c" "b3bcf1b12ef2a7606c7697d71b934ca0bdd495d52f901e73ce008c4c9825a3aa" "f2dd097452b79276ce522df2f8aeb37f6d90f504529616aa46122d549910e46d" default)))
  '(package-selected-packages
    (quote
-    (flycheck avy atom-one-dark-theme zenburn-theme which-key web-mode use-package scss-mode org-bullets json-mode js2-mode counsel company-tern))))
+    (window-numbering zenburn-theme which-key web-mode use-package scss-mode org-bullets json-mode js2-mode flycheck editorconfig counsel company-tern color-theme-modern base16-theme atom-one-dark-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
