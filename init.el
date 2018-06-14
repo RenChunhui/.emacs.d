70e4eb7af24ebc1ba1583ea60f0856635c8845c1
renchunhui <346878847@qq.com>
2018-06-14 10:54:57 +0800

    .emacs.d

--- init.el
+++ init.el
@@ -26,7 +26,6 @@
 (require 'core-emacs)
 (require 'core-packages)
 (require 'core-themes)
-(require 'core-mode-line)
 
 (require 'init-html)
 (require 'init-css)
@@ -37,3 +36,17 @@
 (require 'init-org)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit spaceline zenburn-theme window-numbering which-key web-mode use-package scss-mode org-bullets json-mode js2-mode flycheck editorconfig counsel company-tern color-theme-modern base16-theme atom-one-dark-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
