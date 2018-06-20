c7f2f7e043a99306f94bdf531912e4cb35359f5b
renchunhui <346878847@qq.com>
2018-06-20 15:51:04 +0800

    first commit

--- core/core-themes.el
+++ core/core-themes.el
@@ -1,3 +1,19 @@
+;;; core-themes.el --- themes
+;;
+;; Copyright (c) 2018
+;;
+;; Author  : Chunhui Ren <renchunhui2008@gmail.com>
+;; URL     : https://github.com/RenChunhui/.emacs.d
+;; Version : 1.0.0
+
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
 (use-package zenburn-theme
   :ensure t)
 
@@ -13,3 +29,5 @@
     (load-theme 'atom-one-dark t))
 
 (provide 'core-themes)
+
+;;; core-themes ends here
