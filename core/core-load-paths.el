497f973a2109e38416df6ea5432e99b4b29797b0
renchunhui <346878847@qq.com>
2018-06-26 13:21:11 +0800

    .emacs.d

--- core/core-load-paths.el
+++ core/core-load-paths.el
@@ -1,2 +1,3 @@
 (add-to-list 'load-path (expand-file-name "core" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "extends" user-emacs-directory))
