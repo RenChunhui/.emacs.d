7740f10ad66274ddf10c393cc2dfd30c13458cd3
renchunhui <346878847@qq.com>
2018-07-10 19:41:27 +0800

    .emacs.d

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rjsx-mode yasnippet wttrin dumb-jump ace-jump-mode markdown-mode org-bullets json-mode company-tern tern js2-mode scss-mode web-mode atom-one-dark-theme base16-theme color-theme-modern zenburn-theme magit counsel-projectile projectile editorconfig flycheck counsel which-key company use-package))))
+    (highlight-indent-guides rjsx-mode yasnippet wttrin dumb-jump ace-jump-mode markdown-mode org-bullets json-mode company-tern tern js2-mode scss-mode web-mode atom-one-dark-theme base16-theme color-theme-modern zenburn-theme magit counsel-projectile projectile editorconfig flycheck counsel which-key company use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
