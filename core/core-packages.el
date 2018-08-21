88b2e097bd660d43e3a7f9aa7fc432e99592b1b8
Chunhui Ren <chunhui@ChunhuideMacBook-Pro.local>
2018-08-21 15:32:08 +0800

    change

--- core/core-packages.el
+++ core/core-packages.el
@@ -1,3 +1,24 @@
+;;; core-packages.el --- core packages
+;;
+;; Copyright (c) 2018
+;;
+;; Author  : Chunhui Ren <renchunhui2008@gmail.com>
+;; URL     : https://github.com/RenChunhui/.emacs.d
+;; Version : 1.0.0
+;;
+;;    ___ _ __ ___   __ _  ___ ___
+;;   / _ \ '_ ` _ \ / _` |/ __/ __|
+;;  |  __/ | | | | | (_| | (__\__ \
+;; (_)___|_| |_| |_|\__,_|\___|___/
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'package)
 
 (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
