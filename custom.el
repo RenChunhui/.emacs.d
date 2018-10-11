57968ca6821a025afa38f6f251a9ac7c31b7035c
renchunhui <346878847@qq.com>
2018-10-11 13:09:50 +0800

    add startup screen

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (vimrc-mode tide scss-mode org-bullets markdown-mode json-mode company-tern tern js2-mode web-mode editorconfig magit yasnippet flycheck neotree which-key company winum counsel-projectile counsel evil-indent-textobject evil-surround evil-leader evil use-package memoize))))
+    (osx-dictionary yasnippet winum which-key web-mode vimrc-mode use-package tide scss-mode org-bullets neotree markdown-mode magit json-mode js2-mode evil-leader editorconfig counsel-projectile company-tern))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
