66c10b2e86b1fe665d421c161d8024bd499aadbe
Ren  Chunhui <chunhui@RendeMac-mini.local>
2019-02-11 23:37:54 +0800

    company

---
+++ lisp/init-company.el
@@ -0,0 +1,29 @@
+;;; init-company.el --- Completion with company
+;;
+;; Copyright (c) 2018 Chunhui Ren
+;;
+;; Author  : Chunhui Ren <renchunhui2008@gmail.com>
+;; URL     : https://github.com/RenChunhui/.emacs.d
+;; Version : 1.0.0
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'company)
+
+(setq company-idle-delay 0.1
+      company-tooltip-limit 10
+      company-minimum-prefix-length 1)
+
+(global-set-key (kbd "M-/") 'company-complete)
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+(provide 'init-company)
+
+;;; init-company.el ends here
--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,19 +1,5 @@
-(use-package evil
-  :init
-  (progn
-  	(setq-default evil-vsplit-window-right t
-		evil-split-window-below t
-		evil-normal-state-tag (propertize "NORMAL" 'face '(:foreground "DarkGoldenrod2"))
-		evil-insert-state-tag (propertize "INSERT" 'face '(:foreground "chartreuse3"))
-		evil-visual-state-tag (propertize "VISUAL" 'face '(:foreground "gray"))
-		evil-motion-state-tag (propertize "MOTION" 'face '(:foreground "plum3"))
-		evil-emacs-state-tag (propertize "EMACS" 'face '(:foreground "SkyBlue2")))
-	(add-hook 'after-init-hook 'evil-mode)))
+(require 'evil)
 
-(use-package evil-leader
-  :config
-  (progn
-    (global-evil-leader-mode)
-    (evil-leader/set-leader "<SPC>")))
+(evil-mode 1)
 
 (provide 'init-evil)
