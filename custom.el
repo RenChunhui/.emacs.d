31932701d70ec8cdd13251c89548e3a5c7141cac
Ren  Chunhui <chunhui@RendeMac-mini.local>
2019-06-04 23:54:26 +0800

    submodule

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-bullets yasnippet yaml-mode winum which-key web-mode use-package treemacs-projectile treemacs-magit treemacs-icons-dired treemacs-evil tide scss-mode powerline lsp-ui json-mode js2-mode evil-leader editorconfig dashboard counsel-projectile company-tern company-quickhelp company-lsp company-box benchmark-init atom-one-dark-theme all-the-icons))))
+    (memoize typescript-mode flycheck counsel ivy lsp-mode company treemacs org-bullets yasnippet yaml-mode winum which-key web-mode use-package treemacs-projectile treemacs-magit treemacs-icons-dired treemacs-evil tide scss-mode powerline lsp-ui json-mode js2-mode evil-leader editorconfig dashboard counsel-projectile company-tern company-quickhelp company-lsp company-box benchmark-init atom-one-dark-theme all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -12,30 +12,19 @@
     :ensure t
     :init
     (setq dashboard-startup-banner "~/.emacs.d/private/icons/logo.png"
+	  dashboard-banner-logo-title "Welcome to GNU Emacs."
+	  dashboard-set-footer nil
 	  dashboard-center-content t
 	  dashboard-show-shortcuts nil
 	  dashboard-set-heading-icons t
-    dashboard-set-init-info t
-	  dashboard-set-file-icons nil
-    dashboard-items '((recents  . 5)
-                        (projects . 5)
-                        (agenda . 5)))
+	  dashboard-set-init-info t
+	  dashboard-set-file-icons t
+	  dashboard-items '((recents  . 5)
+			    (projects . 5)
+			    (agenda . 5)))
     :config
     (progn
       (dashboard-setup-startup-hook)))
-
-;; Add heading icons
-(defun dashboard-insert-heading-icon (heading &optional _shortcut)
-  "Add icon to the section's HEADING."
-  (when (and
-         (display-graphic-p)
-         (eq dashboard-set-heading-icons t))
-    (insert (cond
-             ((string-equal heading "Recent Files:") (propertize icon-history 'face 'dashboard-heading))
-             ((string-equal heading "Agenda for today:") (propertize icon-calendar 'face 'dashboard-heading))
-             ((string-equal heading "Projects:") (propertize icon-repo 'face 'dashboard-heading))))
-    (insert " ")))
-(advice-add #'dashboard-insert-heading :before #'dashboard-insert-heading-icon)
 
 (provide 'init-dashboard)
 
