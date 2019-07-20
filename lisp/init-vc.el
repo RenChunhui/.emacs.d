ff78e6580f90b13c4e23ee3fbd3882d561f72c77
Ren Chunhui <346878847@qq.com>
2019-07-20 17:11:12 +0800

    add config

---
+++ lisp/init-vc.el
@@ -0,0 +1,17 @@
+;;; init-vc.el --- vc configuration -*- coding: utf-8; lexical-binding: t -*-
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package magit)
+
+(use-package diff-hl)
+
+(provide 'init-vc)
+
+;;; init-vc.el ends here
